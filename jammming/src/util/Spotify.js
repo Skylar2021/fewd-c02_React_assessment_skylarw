1ba56f159d5d31030a3bcfc73bb45be90471628d
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-06 09:17:00 +0800

    del

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -1,5 +1,4 @@
 // Client ID: f357eb7d14144e5aafe2011b93976ea6
-// Client Secret: 27ed3af612b6402997b6aa6470f65571
 let clientID = f357eb7d14144e5aafe2011b93976ea6
 let token
 const getAccessToken = () =>{
